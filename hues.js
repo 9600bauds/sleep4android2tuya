1d9cf0054e72028f7add70644710d0a20120c53e
9600bauds <blasaladio3@gmail.com>
2023-06-12 16:31:43 -0300

    Better hues

--- hues.js
+++ hues.js
@@ -6,3 +6,37 @@ export const HUE_CYAN = 180;
 export const HUE_BLUE = 240;
 export const HUE_PURPLE = 270;
 export const HUE_MAGENTA = 300;
+
+export const MIN_BRIGHTNESS = 10;
+export const MID_BRIGHTNESS = 100;
+export const MAX_BRIGHTNESS = 1000;
+export const MIN_SATURATION = 10; //mostly just white
+export const MID_SATURATION = 800;
+export const MAX_SATURATION = 1000;
+export const MIN_TEMPERATURE = 10; //orange-ish light
+export const MID_TEMPERATURE = 100;
+export const MAX_TEMPERATURE = 1000; //white light
+
+export const DULL_ORANGE = {
+  hue: HUE_ORANGE,
+  saturation: MAX_SATURATION,
+  brightness: MIN_BRIGHTNESS,
+};
+export const BRIGHT_ORANGE = {
+  hue: HUE_ORANGE,
+  saturation: MID_SATURATION,
+  brightness: MAX_BRIGHTNESS,
+};
+
+export const DIM_WHITE = {
+  temperature: MIN_TEMPERATURE,
+  brightness: MIN_BRIGHTNESS,
+};
+export const MID_WHITE = {
+  temperature: MID_TEMPERATURE,
+  brightness: MID_BRIGHTNESS,
+};
+export const BRIGHT_WHITE = {
+  temperature: MAX_TEMPERATURE,
+  brightness: MAX_BRIGHTNESS,
+};
